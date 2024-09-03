78a6f51a11f5d3fffae5f126f92c68c4ffd802e1
vitorvhsilva <dndragonbr@gmail.com>
2024-09-03 19:52:59 -0300

    Carregar produtos de um arquivo.ts com map

--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../data/listaProdutos";
 
 
 export default function Produtos(){
@@ -8,6 +10,39 @@ export default function Produtos(){
     return(
       <div>
         <h1>Olá, mundo sou o Produtos!</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Marca</th>
+              <th>Descrição</th>
+              <th>Preço</th>
+              <th>Foto</th>
+              <th>Editar | Excluir</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {listaProdutos.map((produto) => (
+              <tr key={produto.id}>
+                <td>{produto.nome}</td>
+                <td>{produto.marca}</td>
+                <td>{produto.desc}</td>
+                <td>{produto.preco}</td>
+                <td><img src={produto.foto} alt={produto.nome} /></td>
+                <td><Link to={`/editar/produtos/${produto.id}`}>Editar</Link></td>
+              </tr>
+            ))}
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan={6}>
+                Total de registros - <span>{listaProdutos.length}</span>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
