846b039d7449c3b556524921cefeac0347ff4dc7
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-03 19:54:58 -0300

    Carregar produtos de um arquivo.ts com map.

--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,4 +1,5 @@
-
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../data/listaProdutos";
 
 export default function Produtos(){
 
@@ -7,7 +8,49 @@ export default function Produtos(){
 
     return(
       <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
+        <h1>Produtos Eletrônicos</h1>
+
+        <table>
+          
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Marca</th>
+              <th>Descrição</th>
+              <th>Preço</th>
+              <th>Foto</th>
+              <th>Editar | Excluir</th>
+            </tr>
+          </thead>
+
+          <tbody>
+              {listaProdutos.map((produto)=>(
+                <tr key={produto.id}>
+                  <td>{produto.nome}</td>
+                  <td>{produto.marca}</td>
+                  <td>{produto.desc}</td>
+                  <td>{produto.preco}</td>
+                  <td><img src={produto.foto} alt={produto.nome}/></td>
+                  <td><Link to={`/editar/produtos/${produto.id}`}>Editar</Link> </td>
+                </tr>
+              ))}
+          </tbody>
+          
+          <tfoot>
+            <tr>
+              <td colSpan={6}>
+                Total de registros - <span>{listaProdutos.length}</span>
+              </td>
+            </tr>
+          </tfoot>
+
+        </table>
       </div>
     );
   }
+  // id: number;
+  // nome: string;
+  // marca: string;
+  // desc: string;
+  // preco: number;
+  // foto: string;
